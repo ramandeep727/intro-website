9bda3649dcd7934071e9f6e8d5f874609d847d82
ramandeep727 <2221037.cse.coe@cgc.edu.in>
2026-03-09 20:12:30 +0530

    Add files via upload

--- datesheet/service-worker.js
+++ datesheet/service-worker.js
@@ -1,9 +1,13 @@
+const CACHE_NAME = "datesheet-cache-v1";
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("datesheet-cache").then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         "/datesheet/login.html",
-        "/datesheet/index.html"
+        "/datesheet/index.html",
+        "/datesheet/script.js",
+        "/datesheet/style.css"
       ]);
     })
   );
@@ -11,6 +15,6 @@ self.addEventListener("install", e => {
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
